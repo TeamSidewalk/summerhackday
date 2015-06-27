db8f02d8943ccdaae05d992db0ea9623afe7cdd2
Emily Chao <elchao96@live.com>
2015-06-26 23:04:51 -0700

    added a route that has a get

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello Leah!'
+
+@app.route('/thing', methods=['GET'])
+def get_thing():
+		if request.method == 'GET':
+			return 'I have a thing'
 
 if __name__ == '__main__':
     app.run()

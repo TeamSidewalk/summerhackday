7a1579d30b1750943de3621eae076e61bdd982c0
evykassirer <evy.kassirer@gmail.com>
2015-06-27 11:31:21 -0700

    nonprofile signup form

--- app.py
+++ app.py
@@ -14,25 +14,24 @@ app.debug = True
 def hello_world():
     return 'Hello Leah!'
 
-@app.route('/thing', methods=['GET'])
-def get_thing():
-    if request.method == 'GET':
-        return 'I have a thing'
-
-@app.route('/search')
-def search():
-    return render_template('search.html')
+@app.route('/nonprofit_signup')
+def signup():
+#    name = request.args.get('name')
+#    name = request.form['name']
+    name = "Khan Academy" 
+    return render_template('nonprofit_signup.html',
+                           name=name)
 
 @app.route('/get_nonprofit_data', methods=['GET', 'POST'])
 def get_nonprofit_data():
-    ein = request.args.get('nonprofit_ein')
+    ein = request.form['nonprofit_ein']
     url = 'https://projects.propublica.org/nonprofits/api/v1/organizations/' + ein + '.json'
     r = requests.get(url)
     return r.text
 
 @app.route('/signup')
 def sign_up():
-		return render_template('signup.html');
+        return render_template('signup.html');
 
 if __name__ == '__main__':
     app.run()

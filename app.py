18b4dff25fdbba8e4aaa757c8fb531f525e63ee3
Daria Jung <djj2115cu@gmail.com>
2015-06-27 11:23:50 -0700

    add some error handling

--- app.py
+++ app.py
@@ -25,10 +25,28 @@ def search():
 
 @app.route('/get_nonprofit_data', methods=['GET', 'POST'])
 def get_nonprofit_data():
+    # default status
+    r = None
+    _dict = {}
+    name = request.args.get('nonprofit_name')
     ein = request.args.get('nonprofit_ein')
-    url = 'https://projects.propublica.org/nonprofits/api/v1/organizations/' + ein + '.json'
-    r = requests.get(url)
-    return r.text
+    url_name = 'https://projects.propublica.org/nonprofits/api/v1/search.json'
+    url_ein = 'https://projects.propublica.org/nonprofits/api/v1/organizations/' + ein + '.json'
+
+    # if given EIN, search with EIN first since unique
+    if ein:
+        r = requests.get(url_ein).text
+    elif name:
+        r = requests.get(url_name, {'q': name}).text
+    if r:
+        _dict = json.loads(r)
+    else:
+        r = json.dumps('No information to search with :C')
+
+    if 'total_results' in _dict and (name or ein):
+        if _dict['total_results'] <= 0:
+            r = "No results for your query. :C"                                                
+    return r
 
 if __name__ == '__main__':
     app.run()
